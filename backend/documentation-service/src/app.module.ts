922107c8a80f97fc489e004dfc232f5564527a1e
wuz1mu <kingaal93@gmail.com>
2023-09-09 02:10:59 +0700

    db config

--- backend/documentation-service/src/app.module.ts
+++ backend/documentation-service/src/app.module.ts
@@ -2,15 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DocumentationModule } from './documentation/documentation.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
 
 @Module({
   imports: [
     DocumentationModule,
     ConfigModule.forRoot({
       load: [configuration],
-    }),],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => configService.get<DataSourceOptions>('db'),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/documentation-service/src/config/configuration.ts
+++ backend/documentation-service/src/config/configuration.ts
@@ -1,3 +1,20 @@
+import { DataSourceOptions } from 'typeorm';
+
+const dbConfig = (): DataSourceOptions => ({
+    type: 'postgres',
+    dropSchema: false,
+    synchronize: false,
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    schema: process.env.DB_SCHEMA,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    migrations: [__dirname + '/../db/migrations/**/*{.ts,.js}'],
+  });
+
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3001,
+    db: dbConfig(),
 });
