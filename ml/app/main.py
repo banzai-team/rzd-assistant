5bf39734ecec452a17b202035669cf7818806e2c
Artem Astapenko <jamakase54@gmail.com>
2023-09-08 22:11:06 +0300

    Fix main.py

--- ml/app/main.py
+++ ml/app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from huggingsound import SpeechRecognitionModel
 
 app = FastAPI()
+
+model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-russian")
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
+class Item:
+    file_path: str
+
+
+@app.post("/transform")
+def speech_to_text(item: Item):
+    transcriptions = model.transcribe([item.file_path])
+    return transcriptions
