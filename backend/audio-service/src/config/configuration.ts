612bbeac49418ae27c836722a215c759781e8f8b
wuz1mu <kingaal93@gmail.com>
2023-09-09 01:04:33 +0700

    update config props. add axios dependency

--- backend/audio-service/src/config/configuration.ts
+++ backend/audio-service/src/config/configuration.ts
@@ -1,15 +1,18 @@
 const helperConfig = () => ({
-    host: process.env.HELPER_HOST
+    host: process.env.HELPER_HOST,
+    port: process.env.HELPER_PORT
 });
 const s2tConfig = () => ({
-    host: process.env.S2T_HOST
+    host: process.env.S2T_HOST,
+    port: process.env.S2T_PORT
 });
 const t2sConfig = () => ({
-    host: process.env.T2S_HOST
+    host: process.env.T2S_HOST,
+    port: process.env.T2S_PORT
 });
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
     helperConfig: helperConfig(),
-    t2sConfig: t2sConfig(),
-    s2tConfig: s2tConfig()
+    t2s: t2sConfig(),
+    s2t: s2tConfig()
 });
