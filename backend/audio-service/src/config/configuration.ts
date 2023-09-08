622e89cb2105eec8835a8cee979bc9794f8b862a
wuz1mu <kingaal93@gmail.com>
2023-09-09 00:43:30 +0700

    configs

--- backend/audio-service/src/config/configuration.ts
+++ backend/audio-service/src/config/configuration.ts
@@ -1,3 +1,15 @@
+const helperConfig = () => ({
+    host: process.env.HELPER_HOST
+});
+const s2tConfig = () => ({
+    host: process.env.S2T_HOST
+});
+const t2sConfig = () => ({
+    host: process.env.T2S_HOST
+});
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
+    helperConfig: helperConfig(),
+    t2sConfig: t2sConfig(),
+    s2tConfig: s2tConfig()
 });
