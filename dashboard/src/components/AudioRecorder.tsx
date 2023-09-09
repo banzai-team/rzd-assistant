a7bb43d33ce339fe53ba34f9a0f67c3cabfe7640
Julia Astapenko <julia.chvyrova@gmail.com>
2023-09-09 23:43:59 +0300

    Add spiner on audio

--- dashboard/src/components/AudioRecorder.tsx
+++ dashboard/src/components/AudioRecorder.tsx
@@ -3,6 +3,7 @@ import { AudioRecorder } from 'react-audio-voice-recorder';
 import {useMutation} from 'react-query';
 import {sendRecord} from '../domain/api';
 import {STORAGE_KEYS} from '../objects';
+import {Spinner} from "@chakra-ui/react";
 
 const Recorder: React.FC = () => {
     const chatId = localStorage.getItem(STORAGE_KEYS.CHAT_ID) ?? 29;
@@ -12,7 +13,11 @@ const Recorder: React.FC = () => {
             console.log('File sent')
         }
     });
-    
+
+    if (send.isLoading) {
+        return <Spinner width="30px" height="30px" ml="10px" />
+    };
+
     return <React.StrictMode>
         <AudioRecorder
             onRecordingComplete={async (blob: Blob) => send.mutate({ file: blob, id: chatId.toString()})}
